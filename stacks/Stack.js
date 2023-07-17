047d8e5b5ae9cb20900d59ebc8671fe5e07ef6b7
Kieran Dunch <dunch.kieran@gmail.com>
2023-07-17 14:40:43 -0700

    WIP: add push pop methods and test

--- stacks/Stack.js
+++ stacks/Stack.js
@@ -9,6 +9,15 @@ class Stack {
     const headData = this.stack.head.data;
     return headData;
   }
+
+  push(data) {
+    this.stack.addToHead(data);
+  }
+
+  pop() {
+    const removedHead = this.stack.removeHead();
+    return removedHead;
+  }
 }
 
 
--- stacks/Stack.test.js
+++ stacks/Stack.test.js
@@ -18,4 +18,30 @@ describe('Stack', () => {
     stack.stack.addToHead('second');
     expect(stack.peek()).toEqual('second');
   });
+
+  test('has a push method', () => {
+    const stack = new Stack();
+    expect(typeof stack.push).toEqual('function');
+  });
+
+  test('push adds an element to the stack', () => {
+    const stack = new Stack();
+    stack.push('first');
+    expect(stack.stack.head.data).toEqual('first');
+    stack.push('second');
+    expect(stack.stack.head.data).toEqual('second');
+  });
+
+  test('has a pop method', () => {
+    const stack = new Stack();
+    expect(typeof stack.pop).toEqual('function');
+  });
+
+  test('pop removes the head element from the stack', () => {
+    const stack = new Stack();
+    stack.push('first');
+    stack.push('second');
+    stack.pop();
+    expect(stack.stack.head.data).toEqual('first');
+  });
 });
