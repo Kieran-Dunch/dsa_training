f10d033baa5e01fd35d36d6882e80264467a8ec4
Kieran Dunch <dunch.kieran@gmail.com>
2023-07-25 07:17:11 -0700

    hash method and testing

--- hashmaps/HashMap.test.js
+++ hashmaps/HashMap.test.js
@@ -2,10 +2,30 @@
 
 const HashMap = require('./HashMap');
 
-
 describe('HashMap', () => {
   it('should create a new hashmap', () => {
     const hashmap = new HashMap();
     expect(hashmap).toBeDefined();
+  });
+
+  // hash method
+  describe('hash', () => {
+    it('should hash a key', () => {
+      const hashmap = new HashMap();
+      const hash = hashmap.hash('key');
+      expect(hash).toBeDefined();
+    });
+
+    it('should hash a key to a number', () => {
+      const hashmap = new HashMap();
+      const hash = hashmap.hash('key');
+      expect(typeof hash).toBe('number');
+    });
+
+    it('should hash a key to a number less than the size of the hashmap', () => {
+      const hashmap = new HashMap(10);
+      const hash = hashmap.hash('key-for-testing-purposes-only');
+      expect(hash).toBeLessThan(hashmap.hashmap.length);
+    });
   });
 });
