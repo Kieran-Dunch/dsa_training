955308fc925e02bfb6d90b607191751e526ec000
Kieran Dunch <dunch.kieran@gmail.com>
2023-08-14 08:06:34 -0600

    add method and tests

--- heaps/MinHeap.js
+++ heaps/MinHeap.js
@@ -3,6 +3,11 @@ class MinHeap {
     this.heap = [null];
     this.size = 0;
   }
+
+  add(value) {
+    this.heap.push(value);
+    this.size++;
+  }
 }
 
 module.exports = MinHeap;
--- heaps/MinHeap.test.js
+++ heaps/MinHeap.test.js
@@ -15,4 +15,17 @@ describe('MinHeap', () => {
     const minHeap = new MinHeap();
     expect(minHeap.heap).toBeDefined();
   });
+
+  describe('add', () => {
+    it('should be a function', () => {
+      const minHeap = new MinHeap();
+      expect(typeof minHeap.add).toBe('function');
+    });
+
+    it('should add a value to the heap', () => {
+      const minHeap = new MinHeap();
+      minHeap.add(1);
+      expect(minHeap.heap.length).toBe(2);
+    });
+  });
 });
