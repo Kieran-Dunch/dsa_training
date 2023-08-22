a65ca16e435460ca87e54b84c9b6ab24e3d5222c
Kieran Dunch <dunch.kieran@gmail.com>
2023-08-22 09:15:21 -0600

    addedge on vertex class

--- graphs/Vertex.js
+++ graphs/Vertex.js
@@ -6,6 +6,17 @@ class Vertex {
     this.edges = [];
   }
 
+  addEdge(vertex, weight) {
+    // check if instance of vertex class
+    if ((vertex instanceof Vertex)) {
+      const newEdge = new Edge(this, vertex);
+      this.edges.push(newEdge);
+      return newEdge;
+    } else {
+      throw new Error('edge start and end must both be vertices');
+    }
+  }
+
   print() {
     const edgeList = this.edges.map(edge => edge.end.data).join(', ');
     console.log(`${this.data} --> ${edgeList}`);
--- graphs/Vertex.test.js
+++ graphs/Vertex.test.js
@@ -6,4 +6,25 @@ describe('Vertex', () => {
     expect(vertex.data).toBe('A');
     expect(vertex.edges).toEqual([]);
   });
+
+  describe('addEdge', () => {
+    it('should be a function', () => {
+      const vertex = new Vertex('A');
+      expect(typeof vertex.addEdge).toEqual('function');
+    });
+
+    it('should add an edge to the vertex', () => {
+      const vertexA = new Vertex('A');
+      const vertexB = new Vertex('B');
+      vertexA.addEdge(vertexB);
+      expect(vertexA.edges.length).toEqual(1);
+      expect(vertexA.edges[0].start).toEqual(vertexA);
+      expect(vertexA.edges[0].end).toEqual(vertexB);
+    });
+
+    it('should throw an error if the argument is not a vertex', () => {
+      const vertex = new Vertex('A');
+      expect(() => vertex.addEdge('B')).toThrow();
+    });
+  });
 });
