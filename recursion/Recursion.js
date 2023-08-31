77a6691516deaeb2fc569fe322e7f5118d73bd55
Kieran Dunch <dunch.kieran@gmail.com>
2023-08-31 07:56:59 -0600

    add base case

--- recursion/Recursion.js
+++ recursion/Recursion.js
@@ -1,10 +1,15 @@
 const recursiveFactorial = (n) => {
+
+  // recursive case
   if (n > 0) {
-    console.log(n);
     return n * recursiveFactorial(n - 1);
   }
-}
 
+  // base case
+  if (n === 0) {
+    return 1;
+  }
+}
 
 module.exports = {
   recursiveFactorial
