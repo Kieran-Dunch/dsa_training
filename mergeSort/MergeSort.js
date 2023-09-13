765c05b57013dcc500af8dd378b484470d3605d5
Kieran Dunch <dunch.kieran@gmail.com>
2023-09-13 08:29:47 -0600

    pseudocode

--- mergeSort/MergeSort.js
+++ mergeSort/MergeSort.js
@@ -1,9 +1,12 @@
 const mergeSort = (arr) => {
+  // base case is when the array is length 1
   if (arr.length < 2) return arr;
 
+  // separate the array into two halves using middle as the pivot
   const middle = Math.floor(arr.length / 2);
   const leftArr = arr.slice(0, middle);
-  const rightArr = arr.slice(middle);
+  const rightArr = arr.slice(middle, arr.length);
 
+  // recursively call mergeSort on each half
   return merge(mergeSort(leftArr), mergeSort(rightArr));
 };
