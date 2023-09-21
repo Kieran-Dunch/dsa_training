d39c0f06bc9df6440c255ee5a6871c7b4cb19307
Kieran Dunch <dunch.kieran@gmail.com>
2023-09-21 07:55:17 -0600

    binary search completed

--- binarySearch/BinarySearch.js
+++ binarySearch/BinarySearch.js
@@ -1,19 +1,18 @@
 const binarySearch = (arr, target) => {
   let left = 0;
   let right = arr.length;
-  let mid = Math.floor((left + right) / 2);
 
-  // while (left <= right) {
-  //   mid = Math.floor((left + right) / 2);
+  while (left < right) {
+    let mid = Math.floor((left + right) / 2);
 
-  //   if (arr[mid] === target) {
-  //     return mid;
-  //   } else if (arr[mid] < target) {
-  //     left = mid + 1;
-  //   } else if (arr[mid] > target) {
-  //     right = mid - 1;
-  //   }
-  // }
+    if (arr[mid] === target) {
+      return mid;
+    } else if (arr[mid] < target) {
+      left = mid + 1;
+    } else if (arr[mid] > target) {
+      right = mid;
+    }
+  }
 
-  // return -1;
+  return null;
 }
