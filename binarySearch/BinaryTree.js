b4c73af6949af9dc46dc416b2fb0b54a8b1f5e39
Kieran Dunch <dunch.kieran@gmail.com>
2023-09-22 10:42:14 -0600

    commentaryy

--- binarySearch/BinaryTree.js
+++ binarySearch/BinaryTree.js
@@ -1,3 +1,10 @@
+// A binary tree is a tree data structure in which each node has at most
+// two children, which are referred to as the left child and the right child.
+// This tree is organized in a hierarchical structure and is used to store data
+// in a sorted manner. Binary trees are commonly used in computer science for
+// searching and sorting operations. The left child of a node is always less
+// than the parent node, and the right child of a node is always greater than
+// the parent. This is called the binary search property.
 class BinaryTree {
   constructor(value, depth = 1) {
     this.value = value;
