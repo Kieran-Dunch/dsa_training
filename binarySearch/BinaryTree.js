0802553cdd059240a71cb53518d37381b27ee0d0
Kieran Dunch <dunch.kieran@gmail.com>
2023-09-22 10:18:14 -0600

    insert method

--- binarySearch/BinaryTree.js
+++ binarySearch/BinaryTree.js
@@ -5,4 +5,20 @@ class BinaryTree {
     this.left = null;
     this.right = null;
   }
+
+  insert(value) {
+    if (value < this.value) {
+      if (!this.left) {
+        this.left = new BinaryTree(value, this.depth + 1);
+      } else {
+        this.left.insert(value);
+      }
+    } else {
+      if (!this.right) {
+        this.right = new BinaryTree(value, this.depth + 1);
+      } else {
+        this.right.insert(value);
+      }
+    }
+  }
 }
