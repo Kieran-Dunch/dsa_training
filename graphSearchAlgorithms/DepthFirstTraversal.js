a8d0fc6569e1b32eba853abf919745954da173ab
Kieran Dunch <dunch.kieran@gmail.com>
2023-09-26 07:34:44 -0600

    DFT one path

---
+++ graphSearchAlgorithms/DepthFirstTraversal.js
@@ -0,0 +1,11 @@
+const graph = require('../graphs/Graph');
+
+const depthFirstTraversal = (start, visitedVertices = [start]) => {
+  start.edges.forEach(edge => {
+    const neighbor = edge.end;
+    if (!visitedVertices.includes(neighbor)) {
+      visitedVertices.push(neighbor);
+      depthFirstTraversal(neighbor, visitedVertices);
+    }
+  });
+};
