4d743f2b5589b3f05bf3762d666c780757b1759d
Kieran Dunch <dunch.kieran@gmail.com>
2023-09-28 06:44:52 -0600

    add callback

--- graphSearchAlgorithms/DepthFirstTraversal.js
+++ graphSearchAlgorithms/DepthFirstTraversal.js
@@ -1,11 +1,11 @@
 const Graph = require('../graphs/Graph');
 
-const depthFirstTraversal = (start, visitedVertices = [start]) => {
+const depthFirstTraversal = (start, callback, visitedVertices = [start]) => {
   start.edges.forEach(edge => {
     const neighbor = edge.end;
     if (!visitedVertices.includes(neighbor)) {
       visitedVertices.push(neighbor);
-      depthFirstTraversal(neighbor, visitedVertices);
+      depthFirstTraversal(neighbor, callback, visitedVertices);
     }
   });
 };
