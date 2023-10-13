f87f10d38e83655696225e03f086b786222f6d01
Kieran Dunch <dunch.kieran@gmail.com>
2023-10-13 07:26:14 -0600

    optimized

--- practiceProblems/Eratosthenes.js
+++ practiceProblems/Eratosthenes.js
@@ -14,6 +14,7 @@
 // 5. Repeat steps 3 and 4 until the counter is greater than the specified
 // maximum value.
 
+// Non-optimized
 const sieveOfEratosthenes = (max) => {
   const output = new Array(max + 1).fill(true);
   output[0] = false;
@@ -33,5 +34,35 @@ const sieveOfEratosthenes = (max) => {
     }
     return acc;
   }, []);
+};
 
+// Optimized
+const sieveOfEratosthenesOpt = (limit) => {
+  // Handle edge cases
+  if (limit <= 1) {
+    return [];
+  }
+  // Create the output
+  const output = new Array(limit + 1).fill(true);
+  // Mark 0 and 1 as non-prime
+  output[0] = false;
+  output[1] = false;
+
+  // Iterate up to the square root of the limit
+  for (let i = 2; i < Math.pow(limit, 0.5); i++) {
+    if (output[i] === true) {
+      // Mark all multiples of i as non-prime
+      for (let j = Math.pow(i, 2); j <= limit; j = j + i) {
+        output[j] = false;
+      }
+    }
+  }
+
+  // Remove non-prime numbers
+  return output.reduce((primes, current, index) => {
+    if (current) {
+      primes.push(index);
+    }
+    return primes
+  }, []);
 };
