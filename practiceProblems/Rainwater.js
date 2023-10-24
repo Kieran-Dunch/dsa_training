38edc3778586e901a82561b615c548513b77501d
Kieran Dunch <dunch.kieran@gmail.com>
2023-10-24 07:34:21 -0600

    naive solution

--- practiceProblems/Rainwater.js
+++ practiceProblems/Rainwater.js
@@ -1,2 +1,22 @@
 // We are implementing a solution to finding how much rainwater can be trapped
 // in a given array of heights.
+
+
+// naive solution
+// time complexity: O(n^2)
+
+const naiveRainwater = (heights) => {
+  let totalWater = 0;
+  for (let i = 0; i < heights.length - 1; i++) {
+    let leftMax = 0;
+    let rightMax = 0;
+    for (let j = 0; j <= i; j++) {
+      leftMax = Math.max(leftMax, heights[j]);
+    }
+    for (let j = i; j < heights.length; j++) {
+      rightMax = Math.max(rightMax, heights[j]);
+    }
+    totalWater += Math.min(leftMax, rightMax) - heights[i];
+  }
+  return totalWater;
+}
